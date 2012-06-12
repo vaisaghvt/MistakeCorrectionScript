2e50b0a3c540edb8a142d249300527dcbeadc146
Vaisagh <vaisaghvt@gmail.com>
2012-06-13 00:40:21 +0800

    Ability to read command line arguments
    Read the command line arguments and correct mistakes in each file in
    turn.

--- readPara.py
+++ readPara.py
@@ -5,9 +5,7 @@ import Getch as gc
 from patterns import * 
 from extractPhrases import *
 from tagChecks import *
-
-fileName = "random.txt"
-
+import sys
 
 def dealWithIt(match, para, replacementFunction):
     replacement =  suggestReplacement(match, para, replacementFunction)
@@ -45,50 +43,55 @@ def writeToFile(group):
 
 
 # paragraph_lists = []
-
-group = []
-with open(fileName) as f:
-    paras = paragraphs(f)
-    for para in paras:
-        group.append(para)
-count=0
-# print group
-newContent =""
-matched = False
-for iteration in range(1,3,1):
-    for pattern in patterns.keys():
-        # print count 
-        count=count+1
-        flag = False
-        for index, para in enumerate(group):
-            regex = re.compile(pattern)
-            # m = re.findall(pattern,line)
-        #   writeFile.write(line.replace("tutu","random"))
-            replaced = True
-            while (replaced):
-                if replaced:
-                    replaced = False;
-                for match in regex.finditer(para):
-                    for option in patterns[pattern][1]:
-                        if(checkPattern(option, match, para)):
-                            flag= True
-                            break
-                    if flag:
-                        flag = False
-                        continue
-                    matched = True
-                    print "Problem: ", patterns[pattern][0],"; Para", count
-                    print "Phrase: ", extractPhrase(match,para)
-                    # print "context: ",para
-                    group[index], replaced = dealWithIt(match, para, patterns[pattern][2])
-                    para= group[index]            
-                    print "**********************"
+for fileName in sys.argv[1:]:
+    print "//////////////Checking ", fileName, "/////////////////////"
+    print "********************"
+    group = []
+    with open(fileName) as f:
+        paras = paragraphs(f)
+        for para in paras:
+            group.append(para)
+    count=0
+    # print group
+    newContent =""
+    matched = False
+    for iteration in range(1,3,1):
+        for pattern in patterns.keys():
+            # print count 
+            count=count+1
+            flag = False
+            for index, para in enumerate(group):
+                regex = re.compile(pattern)
+                # m = re.findall(pattern,line)
+            #   writeFile.write(line.replace("tutu","random"))
+                replaced = True
+                while (replaced):
                     if replaced:
-                        break
+                        replaced = False;
+                    for match in regex.finditer(para):
+                        for option in patterns[pattern][1]:
+                            if(checkPattern(option, match, para)):
+                                flag= True
+                                break
+                        if flag:
+                            flag = False
+                            continue
+                        matched = True
+                        print "Problem: ", patterns[pattern][0],"; Para", count
+                        print "Phrase: ", extractPhrase(match,para)
+                        # print "context: ",para
+                        group[index], replaced = dealWithIt(match, para, patterns[pattern][2])
+                        para= group[index]            
+                        print "**********************"
+                        if replaced:
+                            break
+    if matched == False:
+        print "No mistakes found. Good Stuff!"
+    else:
+        print "Corrections made in ", fileName
+        writeToFile(group)
+        matched = False
 
-if matched != True:
-    print "No mistakes found. Good Stuff!"
-writeToFile(group)
 
 
 
